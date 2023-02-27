ff575d93e9510e67a031abc0cdd4faf3ad43aed0
jecndlria <jecndlria@gmail.com>
2023-02-26 20:06:08 -0800

    finished test parser

---
+++ manifest/manifest.cpp
@@ -0,0 +1,7 @@
+#include "manifest.h"
+
+int main(int argc, char *argv[])
+{
+   parseTest();
+   return 0;
+}
---
+++ manifest/manifest.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+/*
+Format of each manifest line:
+[RowInt, ColumnInt], {WeightInt}, Name/NAN/Unused
+Example: [01, 01], {00000}, NAN
+
+This parse is mainly proof of concept; we can easily parse through a manifest and extract information.
+This test simply gets the information from each row, and outputs it.
+*/
+
+void parseTest()
+{
+   // Get the manifest name, and open it.
+   std::string manifest;
+   std::cout << "Enter name of manifest: ";
+   std::getline(std::cin, manifest);
+   std::cout << "\nParsing " << manifest << "...\n";
+   std::ifstream file(manifest);
+
+   // Parse through the manifest, getting all necessary information.
+   if (file.is_open())
+   {
+      char c;
+      int row, column, weight;
+      std::string name, line;
+
+      while (std::getline(file, line))
+      {
+         // std::cout << line << '\n'; // Debug
+         std::stringstream s(line);
+         s >> std::skipws >> c >> row >> c >> column >> c >> c >> c >> weight >> c >> c >> name;
+         std::cout << "Row: " << row << " Column: " << column << " Weight: " << weight << " Name: " << name;
+         std::cout << '\n';
+      }
+      file.close();
+   }
+   else
+   {
+      std::cout << "ERROR: Unable to open file.\n";
+      return;
+   }
+
+   std::cout << "Successfully parsed through " << manifest << ".\n";
+}
