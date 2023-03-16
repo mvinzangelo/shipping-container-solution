69616a4b883dd7fc1f99b09f941c788101989232
jecndlria <jecndlria@gmail.com>
2023-03-16 23:39:24 +0000

    timestamp works

--- logfile/logfile.cpp
+++ logfile/logfile.cpp
@@ -1,6 +1,9 @@
 #include "logfile.h"
+#include <string>
 
 int main(int argc, char *argv[])
 {
-    initializeLogFile();
+    LogFile log = LogFile();
+    std::string name = "Joshua Candelaria";
+    log.logEmployeeCheckIn(name);
 }
--- logfile/logfile.h
+++ logfile/logfile.h
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
 
+// Macros to make it easy to define onload/offload to log
 #define ONLOAD 0
 #define OFFLOAD 1
 
-class LogFile 
+class LogFile
 {
     private:
         std::ofstream logFile /*= "KeoghLongBeach.txt"*/;
     public:
-        LogFile() : logFile("KeoghLongBeach.txt");
+        LogFile() : logFile("KeoghLongBeach.txt") {};
         void logEmployeeCheckIn(std::string&);
         void logAtomicMove(std::string&, int);
         void logManifestOpen();
@@ -18,3 +20,15 @@ class LogFile
         void getOperatorMessage();
         void restartLogFile();
 };
+
+void LogFile::logEmployeeCheckIn(std::string& name)
+{
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_t = std::chrono::system_clock::to_time_t(now);
+    std::tm tm = *std::localtime(&now_t);
+
+    char buffer[80];
+    std::strftime(buffer, 80, "%B %d, %Y: %H:%M:%S", &tm);
+
+    this->logFile << buffer << ": " << name << " has checked in.\n";
+}
