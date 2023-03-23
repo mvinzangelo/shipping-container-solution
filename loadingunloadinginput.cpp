3d35c7afaedf4ea1a80589bb44932fad3a3a506c
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-03-23 10:41:22 -0700

    Add loadingunloadinginput class

---
+++ loadingunloadinginput.cpp
@@ -0,0 +1,6 @@
+#include "loadingunloadinginput.h"
+
+LoadingUnloadingInput::LoadingUnloadingInput()
+{
+
+}
---
+++ loadingunloadinginput.h
@@ -0,0 +1,11 @@
+#ifndef LOADINGUNLOADINGINPUT_H
+#define LOADINGUNLOADINGINPUT_H
+
+
+class LoadingUnloadingInput
+{
+public:
+    LoadingUnloadingInput();
+};
+
+#endif // LOADINGUNLOADINGINPUT_H
