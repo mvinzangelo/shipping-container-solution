c9128b75ec929ee1473feb8aec85c6e10ddc3f23
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-03-22 18:31:20 -0700

    Add variables

--- containercell.cpp
+++ containercell.cpp
@@ -2,6 +2,16 @@
 
 ContainerCell::ContainerCell(QWidget *parent) : QPushButton(parent)
 {
-    setFixedHeight(50);
-    setFixedWidth(50);
+    setFixedHeight(80);
+    setFixedWidth(80);
+    QColor cellColor = Qt::white;
+    QColor hoverColor = Qt::red;
+    QString cs1=QString("QPushButton {"
+                   "background-color: %1;"
+                   "color: rgb(0, 0, 0);"
+                 "}"
+                 "QPushButton:hover {"
+                     "background-color: %2;"
+                 "}").arg(cellColor.name(), hoverColor.name());
+    setStyleSheet(cs1);
 }
