23d1c0aaa43cf2b6ccb14923b03a9cd2ebf47cc2
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2023-03-23 17:12:37 -0700

    Fix compilation with external things

--- logfile/logfile.cpp
+++ logfile/logfile.cpp
@@ -152,21 +152,21 @@ void LogFile::getOperatorMessage()
 }
 
 
-int main(int argc, char *argv[])
-{
-    LogFile log;
-    std::string name = "Joshua Candelaria";
-    std::string container = "Container";
-    std::string manifestName;
-    std::cout << "Input a manifest name: ";
-    std::getline(std::cin >> std::ws, manifestName);
-    std::cout << '\n';
-    Ship currentShip(manifestName);
+//int main(int argc, char *argv[])
+//{
+//    LogFile log;
+//    std::string name = "Joshua Candelaria";
+//    std::string container = "Container";
+//    std::string manifestName;
+//    std::cout << "Input a manifest name: ";
+//    std::getline(std::cin >> std::ws, manifestName);
+//    std::cout << '\n';
+//    Ship currentShip(manifestName);
     
-    log.logEmployeeCheckIn(name);
-    log.logAtomicMove(container, ONLOAD);
-    log.getOperatorMessage();
-    log.logAtomicMove(container, OFFLOAD);
-    log.logManifestOpen(currentShip);
-    log.logManifestFinish(currentShip);
-}
+//    log.logEmployeeCheckIn(name);
+//    log.logAtomicMove(container, ONLOAD);
+//    log.getOperatorMessage();
+//    log.logAtomicMove(container, OFFLOAD);
+//    log.logManifestOpen(currentShip);
+//    log.logManifestFinish(currentShip);
+//}
